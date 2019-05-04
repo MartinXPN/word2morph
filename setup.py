b7359a3fd26483ce856c445cceb0439934c3d635
MartinXPN <mirakyanmartin@gmail.com>
2019-05-05 02:24:31 +0400

    removed keras-contrib version

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
         'scikit-learn>=0.20.2',
         'tensorflow>=1.12.0',
         'tqdm>=4.31.1',
-        'keras-contrib @ git+https://www.github.com/keras-team/keras-contrib',
+        'keras-contrib @ git+https://www.github.com/keras-team/keras-contrib.git',
     ],
     extras_require={},
     include_package_data=True,
