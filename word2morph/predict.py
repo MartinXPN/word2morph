823d8a734219cd83d21ea4ba167520e30fa5192e
MartinXPN <mirakyanmartin@gmail.com>
2019-05-11 23:14:06 +0400

    fixed beam search (penalty function add logs instead of multiplying)

--- word2morph/predict.py
+++ word2morph/predict.py
@@ -1,17 +1,12 @@
 import fire
 
 from word2morph import Word2Morph
-from word2morph.data.beamsearch import PostProcessBeamSearch
 from word2morph.data.loaders import DataLoader
 
 
 def predict(model_path: str, batch_size: int = 1,
             input_path='datasets/rus.test', output_path='logs/rus.predictions'):
     word2morph = Word2Morph.load_model(path=model_path)
-    word2morph.processor = PostProcessBeamSearch(char_mapping=word2morph.processor.char_mapping,
-                                                 word_segment_mapping=word2morph.processor.word_segment_mapping,
-                                                 bmes_mapping=word2morph.processor.bmes_mapping,
-                                                 label_mapping=word2morph.processor.label_mapping)
     inputs = DataLoader(file_path=input_path).load()
     correct, wrong, predicted_samples = word2morph.evaluate(inputs, batch_size=batch_size)
     with open(output_path, 'w', encoding='utf-8') as f:
