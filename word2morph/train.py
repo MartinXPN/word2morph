bff4911dc52601b01704f1f02eef749b1397ea06
Ann Simonyan <mirakyanmartin@gmail.com>
2019-06-04 21:22:36 -0700

    added learning rate beam search support

--- word2morph/train.py
+++ word2morph/train.py
@@ -1,5 +1,7 @@
 import json
+import os
 import sys
+from copy import copy
 from datetime import datetime
 from itertools import chain
 from pathlib import Path
@@ -151,5 +153,85 @@ class Gym(object):
             .train(**map_arguments(self.train, kwargs))
 
 
+class ModelInstance:
+    def __init__(self, performance, path, lr):
+        self.performance = performance
+        self.path = path
+        self.lr = lr
+
+    def __eq__(self, other: 'ModelInstance'):
+        return self.performance, self.path, self.lr == other.performance, other.path, other.lr
+
+    def __lt__(self, other: 'ModelInstance'):
+        return self.performance < other.performance
+
+    def __str__(self):
+        return f'Model with perf: {self.performance}, lr: {self.lr}, saved at: {self.path}'
+
+
+class LearningRateBeamSearchGym(Gym):
+
+    def train(self, batch_size: int = 32, epochs: int = 100,
+              lr_multipliers: Tuple[float, ...] = (0.5, 0.75, 0.8, 1, 1.2, 1.5, 2), nb_models: int = 3,
+              threads: int = 4,
+              monitor_metric: str = 'val_word_acc_processed', log_dir: str = 'logs',
+              **kwargs):
+        self.params.update(locals()), self.params.pop('self')
+
+        ''' Save all the objects/parameters for reproducibility '''
+        log_dir = Path(log_dir).joinpath(datetime.now().replace(microsecond=0).isoformat())
+        model_path = Path(log_dir).joinpath('checkpoints').joinpath("best-model.joblib")
+        model_path.parent.mkdir(parents=True, exist_ok=True)
+        with open(Path(log_dir).joinpath('params.json'), 'w', encoding='utf-8') as f:
+            json.dump({'params': self.params, 'commandline': sys.argv, 'commit': get_current_commit()}, f, indent=4)
+
+        train_generator = DataGenerator(dataset=self.train_dataset, processor=self.processor, batch_size=batch_size)
+        valid_generator = DataGenerator(dataset=self.valid_dataset, processor=self.processor, batch_size=batch_size,
+                                        with_samples=True)
+
+        best_current_models: List[ModelInstance] = []
+        best_prev_models: List[ModelInstance] = []
+
+        for epoch in range(epochs):
+            best_prev_models = copy(best_current_models)
+            best_current_models = []
+
+            def log_model(score):
+                nonlocal best_current_models
+                path = f'{log_dir}/model-epoch:{epoch}-acc:{score:.3f}.joblib'
+                best_current_models.append(ModelInstance(performance=score,
+                                                         path=path,
+                                                         lr=float(K.get_value(self.model.optimizer.lr))))
+                print('Obtained:', str(best_current_models[-1]), flush=True)
+                Word2Morph(model=self.model, processor=self.processor).save(path)
+
+                best_current_models = sorted(best_current_models, reverse=True)
+                best_current_models, worst = best_current_models[:nb_models], best_current_models[nb_models:]
+                for model in worst:
+                    if Path(model.path).exists():
+                        os.remove(model.path)
+
+            # There are no models for the initial epoch => use the initial random model as the base model
+            if len(best_current_models) == 0:
+                log_model(score=0)
+
+            for base_model in best_prev_models:
+                for lr_multiplier in lr_multipliers:
+                    print('Trying to modify:', str(base_model), flush=True)
+                    w2m = Word2Morph.load_model(base_model.path)
+                    self.model, self.processor = w2m.model, w2m.processor
+                    lr = float(K.get_value(self.model.optimizer.lr))
+                    K.set_value(self.model.optimizer.lr, lr * lr_multiplier)
+
+                    history = self.model.fit_generator(
+                        generator=train_generator,
+                        epochs=epoch + 1, initial_epoch=epoch,
+                        callbacks=[Evaluate(data_generator=valid_generator, to_sample=self.processor.to_sample)],
+                        class_weight=self.class_weights,
+                        use_multiprocessing=True, workers=threads,
+                    )
+                    log_model(score=history.history[monitor_metric][-1])
+
+
 if __name__ == '__main__':
-    fire.Fire(Gym)
+    fire.Fire(LearningRateBeamSearchGym)
